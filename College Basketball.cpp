078e129c69c03ab569da60a1de02f833709a6c68
lacarp99 <lacarp99@gmail.com>
2020-05-20 12:14:46 -0400

    Added player names. Added one random skill level generator. Added Coach name.

--- College Basketball.cpp
+++ College Basketball.cpp
@@ -1,36 +1,162 @@
 #include <cstdio>
 #include <cstdlib>
 #include <iostream>
+#include <random>
+#include <time.h>
 
 using namespace std;
 
 int chosenTeam = 0;
+int skill = NULL;
 
+struct newcoach {
+	string firstname;
+	string lastname;
+} coach ;
+
+//Teams Names
 const char* teams[14] = {
-	"1.  Michigan",
-	"2.  Michigan State",
-	"3.  Ohio State",
-	"4.  Maryland",
-	"5.  Wisconsin",
-	"6.  Illinois",
-	"7.  Penn State",
-	"8.  Iowa",
-	"9.  Rutgers",
-	"10. Indiana",
-	"11. Purdue",
-	"12. Minnesota",
-	"13. Northwestern",
-	"14. Nebraska"
+	"Michigan",
+	"Michigan State",
+	"Ohio State",
+	"Maryland",
+	"Wisconsin",
+	"Illinois",
+	"Penn State",
+	"Iowa",
+	"Rutgers",
+	"Indiana",
+	"Purdue",
+	"Minnesota",
+	"Northwestern",
+	"Nebraska"
+};
+
+//Possible Player Names
+const string names[100] = {
+	"Smith"
+	,"Johnson"
+	,"Williams"
+	,"Jones"
+	,"Brown"
+	,"Davis"
+	,"Miller"
+	,"Wilson"
+	,"Moore"
+	,"Taylor"
+	,"Anderson"
+	,"Thomas"
+	,"Jackson"
+	,"White"
+	,"Harris"
+	,"Martin"
+	,"Thompson"
+	,"Garcia"
+	,"Martinez"
+	,"Robinson"
+	,"Clark"
+	,"Rodriguez"
+	,"Lewis"
+	,"Lee"
+	,"Walker"
+	,"Hall"
+	,"Allen"
+	,"Young"
+	,"Hernandez"
+	,"King"
+	,"Wright"
+	,"Lopez"
+	,"Hill"
+	,"Scott"
+	,"Green"
+	,"Adams"
+	,"Baker"
+	,"Gonzalez"
+	,"Nelson"
+	,"Carter"
+	,"Mitchell"
+	,"Perez"
+	,"Roberts"
+	,"Turner"
+	,"Phillips"
+	,"Campbell"
+	,"Parker"
+	,"Evans"
+	,"Edwards"
+	,"Collins"
+	,"Stewart"
+	,"Sanchez"
+	,"Morris"
+	,"Rogers"
+	,"Reed"
+	,"Cook"
+	,"Morgan"
+	,"Bell"
+	,"Murphy"
+	,"Bailey"
+	,"Rivera"
+	,"Cooper"
+	,"Richardson"
+	,"Cox"
+	,"Howard"
+	,"Ward"
+	,"Torres"
+	,"Peterson"
+	,"Gray"
+	,"Ramirez"
+	,"James"
+	,"Watson"
+	,"Brooks"
+	,"Kelly"
+	,"Sanders"
+	,"Price"
+	,"Bennett"
+	,"Wood"
+	,"Barnes"
+	,"Ross"
+	,"Henderson"
+	,"Coleman"
+	,"Jenkins"
+	,"Perry"
+	,"Powell"
+	,"Long"
+	,"Patterson"
+	,"Hughes"
+	,"Flores"
+	,"Washington"
+	,"Butler"
+	,"Simmons"
+	,"Foster"
+	,"Gonzales"
+	,"Bryant"
+	,"Alexander"
+	,"Russell"
+	,"Griffin"
+	,"Diaz"
+	,"Hayes"
 };
 
 int main(int nNumberofArgs, char* pszArgs[])
 {
+	//Setup
+	srand(time(NULL));
+	//Welcome Player
 	cout << "Welcome to Big 10 College Basketball! Select a team from the following list:" << endl;
 	for (int i = 0; teams[i] != NULL; i++) {
-		cout << teams[i] << endl;
+		cout << i+1 << ". " << teams[i] << endl;
 	}
+	//Get player's team of choice
 	cout << endl << "Type the number of the team you would like to play as: ";
 	cin >> chosenTeam;
 	cout << "You chose: " << teams[chosenTeam-1] << endl;
+	//Get name of player (coach)
+	cout << "What is the name of your coach?" << endl;
+	cout << "Coach: ";
+	cin >> coach.firstname;
+	cin >> coach.lastname;
+	cout << "Welcome Coach " << coach.lastname << " we're glad to have you here at " << teams[chosenTeam - 1] << endl;
+	//Create team and save random names
+	skill = rand() % 3;
+	cout << skill << endl;
 	return 0;
 }
